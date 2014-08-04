6f79153c8f10fd03f6a29436031d26933d2e3574
vylgin <vylginvv@gmail.com>
2014-08-05 01:26:07 +0400

    Initial commit

---
+++ app/src/main/java/pro/vylgin/getcollage/MainActivity.java
@@ -0,0 +1,41 @@
+package pro.vylgin.getcollage;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
+
+
+public class MainActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+        return id == R.id.action_settings || super.onOptionsItemSelected(item);
+    }
+
+    public void getCollage(View view) {
+        String instagramUser = ((EditText) findViewById(R.id.instagramUserEditText)).getText().toString();
+        Toast.makeText(this, String.format("Get Collage, %s!", instagramUser), Toast.LENGTH_SHORT).show();
+    }
+}
